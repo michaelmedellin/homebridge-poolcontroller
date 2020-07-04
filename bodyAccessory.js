f11269e47d8d67b4cd964dc062906a7c0982192e
Parth Jhaveri <parthj@MacBook-Pro.local>
2020-07-04 11:12:01 -0700

    added Pump display

--- bodyAccessory.js
+++ bodyAccessory.js
@@ -141,7 +141,8 @@ PoolBodyAccessory.prototype.setThermoTargetState = function(newTargetState, call
 PoolBodyAccessory.prototype.updateState = function(newbodyData) {
 
     this.bodyData = newbodyData;
-
+    this.platform.log('Updating body values - heat status ', this.bodyData.heatStatus)
+    this.platform.log('Translated ', utils.HeatingState(this.bodyData.heatStatus, Characteristic))
     this.accessory.getService(Service.Switch).getCharacteristic(Characteristic.On)
       .updateValue(this.bodyData.isOn); 
 
